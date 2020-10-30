afd93778f63413a6f78897c378683567a84a92b2
Yaser <yaser.kazerooni@gmail.com>
2020-10-30 16:52:36 +0100

    Add App Config to handle inline bean class

---
+++ src/main/java/com/mondiamedia/app/AppConfig.java
@@ -0,0 +1,33 @@
+package com.mondiamedia.app;
+
+import com.mondiamedia.app.security.AppProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Yaser Kazerooni (yaser.kazerooni@gmail.com)
+ * @version 1.0 2020.10.30
+ * @since 1.0
+ */
+@Configuration
+public class AppConfig {
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+
+  @Bean
+  public SpringApplicationContext springApplicationContext() {
+    return new SpringApplicationContext();
+  }
+
+  @Bean(name="AppProperties")
+  public AppProperties appProperties() {
+    return new AppProperties();
+  }
+
+}
